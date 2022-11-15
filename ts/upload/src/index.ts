f2b877ad292ea663cd4ad35366462c280302cdf3
terry <terry@fromsmash.com>
2022-11-15 14:58:25 +0100

    added more parameters in upload example

--- ts/upload/src/index.ts
+++ ts/upload/src/index.ts
@@ -7,7 +7,31 @@ const files = [
     "./dummyFiles/dummy2.png",
     "./dummyFiles/dummy3.txt",
 ];
-su.upload({ files }).then(transfer => {
+su.upload({
+    files,
+    language: 'en',
+    title: 'my-transfer',
+    description: 'my-description',
+    password: 'my-password',
+    availabilityDuration: 86400,
+    customization: {
+        logo: 'https://my-logo-url.com',
+        background: 'https://my-background-url.com',
+    },
+    notificationType: 'All',
+    preview: 'Full',
+    delivery: {
+        type: 'Link',
+        sender: {
+            name: 'Firstname Lastname',
+            email: 'email@test.com',
+        },
+        receivers: [
+            'email1@test.com',
+            'email2@test.com'
+        ],
+    },
+}).then(transfer => {
     console.log(transfer);
 }).catch(error => {
     console.log(error);
