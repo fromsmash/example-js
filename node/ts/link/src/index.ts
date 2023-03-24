d3f703b14713bffa8ee7494182eac59dc1b0e73a
AzureAD\EmmanuelNeny <emmanuel@fromsmash.com>
2023-03-24 11:46:37 +0100

    don't pass region to link

--- node/ts/link/src/index.ts
+++ node/ts/link/src/index.ts
@@ -1,7 +1,7 @@
 
 import { Link } from "@smash-sdk/link/10-2019";
 
-const linkSdk = new Link({ region: "eu-west-3", token: "Put your api key here" })
+const linkSdk = new Link({ token: "Put your api key here" })
 
 linkSdk.getTarget({
     targetId: 'url',
