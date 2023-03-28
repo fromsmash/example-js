7e59525b868e7aebe475c215b6aebf59c00a345e
terry <terry@fromsmash.com>
2023-03-28 18:05:05 +0200

    fix deps and examples

--- node/ts/downloader/src/advanced.ts
+++ node/ts/downloader/src/advanced.ts
@@ -2,9 +2,10 @@
 import { SmashDownloader } from "@smash-sdk/downloader";
 
 const sd1 = new SmashDownloader({
-    token: "Put api key here",
-    url: "Smash url like https://fromsmash.com/my-smash or https://my-domain.fromsmash.com/my-smash-on-subdomain",
-    path: "Path to file, ex: ./dummyFiles/dummy.zip",
+    token: "Put your api key",
+    transferId: "Put a transfer id here",
+    path: "Path to file, ex: /dummyFiles/dummy.zip",
+    password: "Put a password here", // this field corresponds to the password of the transfer
     //enableOverride: true, // support for override
 });
 
