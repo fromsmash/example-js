b4d7573978cb00328c4a679d447502cca52bae88
terry <terry@fromsmash.com>
2023-07-18 18:03:02 +0200

    wip

--- node/ts/uploader/src/buffer.ts
+++ node/ts/uploader/src/buffer.ts
@@ -3,9 +3,9 @@ import { SmashUploader } from "@smash-sdk/uploader";
 
 const su = new SmashUploader({ region: "eu-west-3", token: "Put your api key here" })
 const files = [
-    { name: "tree.png", content: Buffer.from("This is a first string") },
-    { name: "folder1/tree.png", content: Buffer.from("This is second string") },
-    { name: "folder2/folder3/tree.txt", content:  Buffer.from("This is a third string") },
+    { name: "buffer.txt", content: Buffer.from("This is a first string") },
+    { name: "folder1/buffer.txt", content: Buffer.from("This is second string") },
+    { name: "folder2/folder3/buffer.txt", content:  Buffer.from("This is a third string") },
 ];
 su.upload({
     files,
