5ae8dc9a99c65a361b9cf3749d3f5d4ff95e9d10
terry <terry@fromsmash.com>
2023-07-18 18:02:59 +0200

    <ip

--- node/ts/uploader/src/string.ts
+++ node/ts/uploader/src/string.ts
@@ -3,9 +3,9 @@ import { SmashUploader } from "@smash-sdk/uploader";
 
 const su = new SmashUploader({ region: "eu-west-3", token: "Put your api key here" })
 const files = [
-    { name: "tree.png", content: "This is a first string" },
-    { name: "folder1/tree.png", content: "This is a second string" },
-    { name: "folder2/folder3/tree.txt", content: "This is a third string" },
+    { name: "text.txt", content: "This is a first string" },
+    { name: "folder1/text.txt", content: "This is a second string" },
+    { name: "folder2/folder3/text.txt", content: "This is a third string" },
 ];
 su.upload({
     files,
