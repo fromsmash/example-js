9e9c8e87ebfb89834811e55e467f41051b7679c8
terry <terry@fromsmash.com>
2024-11-29 17:10:49 +0100

    add example to download files withtout zip and to download a specifi file

---
+++ node/ts/downloader/src/download_unzipped_files.ts
@@ -0,0 +1,44 @@
+
+import { SmashDownloader } from "@smash-sdk/downloader";
+import { ListTransferFilesOutput, Transfer } from "@smash-sdk/transfer/01-2024";
+import Bluebird from "bluebird";
+
+const token = "Put your Smash API key";
+
+async function listTransferFiles(transferId: string): Promise<ListTransferFilesOutput['files']> {
+    const transferSdk = new Transfer({
+        token,
+        region: "eu-west-3",
+    });
+    const files = await transferSdk.listTransferFiles({ transferId });
+    while (files.next) {
+        const nextFiles = await transferSdk.listTransferFiles({ transferId, start: files.next });
+        files.files.push(...nextFiles.files);
+        files.next = nextFiles.next;
+    }
+    return files.files;
+}
+
+async function downloadFile({ transferId, fileId, fileName }: { transferId: string, fileId: string, fileName: string }) {
+    const downloader = new SmashDownloader({
+        token,
+        transferId,
+        fileId,
+        path: `./${fileName}`, // path to save the file
+    });
+    const file = await downloader.download();
+    return file;
+}
+
+async function download(transferId: string) {
+    const files = await listTransferFiles(transferId);
+    await Bluebird.map(files, async (file) => {
+        await downloadFile({ transferId, fileId: file.id, fileName: file.name });
+    }, { concurrency: 5 });
+}
+
+download("Put a transfer id here").then(() => {
+    console.log("Download completed");
+}).catch((error) => {
+    console.error("Error", error);
+});
